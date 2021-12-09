73f25af9eef9d2654769c0871e6f408072f90072
DanDemon666 <dandemon13@mail.ru>
2021-12-09 14:40:14 +0300

    Work

---
+++ myscript1.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "hello Gei"
+cd ../../
+cd /D
+mkdir tut
+cd /D/tut
+mkdir dier_1 dier_2 dier_3
+cd dier_1
+touch tesst_1.txt tesst_2.txt tesst_3.txt tesst_4.json tesst_5.json
+mkdir Sossi_1 Sossi_2 Sossi_3
+mv tesst_4.json Sossi_3
+mv tesst_1.txt Sossi_3
